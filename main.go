a742d37940857a89aea6c1283ff620c978437406
Michał Mieczkowski <mieczkowski@fastmail.com>
2023-06-30 18:29:16 +0200

    get rid of annoying macOs network prompt

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("127.0.0.1:3000", nil)
 }
