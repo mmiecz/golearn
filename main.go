527f3498d3f5749290141b602dc215bf7d1bbcad
Michał Mieczkowski <mieczkowski@fastmail.com>
2023-06-30 19:43:56 +0200

    templating, and chi router

--- main.go
+++ main.go
@@ -2,26 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		handleHome(w, r)
-	case "/contact":
-		handleContact(w, r)
-	default:
-		handleNotFound(w, r)
-	}
-}
-
-func handleNotFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "Page Not Found!")
-}
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Awesome GO!</h1>")
@@ -31,9 +16,20 @@ func handleContact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Info</h1><p>email here</p>")
 }
+func handleGallery(w http.ResponseWriter, r *http.Request) {
+	galleryUid := chi.URLParam(r, "galleryUid")
+	fmt.Fprintf(w, "GET gallery %v", galleryUid)
+}
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", handleHome)
+	r.Get("/contact", handleContact)
+	r.Get("/gallery/{galleryUid}", handleGallery)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe("127.0.0.1:3000", router)
+	http.ListenAndServe("127.0.0.1:3000", r)
 }
